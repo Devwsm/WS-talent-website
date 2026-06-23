95c818c6255efc54c99e72e59fe3bc3bc26377aa
arga <it.wsm2026@gmail.com>
2026-06-23 11:47:26 +0700

    news navbar

--- resources/views/components/dashboard/navbar.blade.php
+++ resources/views/components/dashboard/navbar.blade.php
@@ -1,16 +1,20 @@
 <div class="nav z-50 fixed w-fit flex gap-2 bottom-5">
     <div class="nav-links text-3xl flex justify-center items-center gap-4 p-6 rounded-lg bg-[#1A1A1B]">
         <a href="{{ route('dashboard') }}" class="text-[#F5F1E6] hover:text-[#d5ccb3] text-[1.5rem] md:text-[2rem] ">
-            <i class="bi bi-house-door-fill me-2"></i>
+            <i class="bi bi-house-door-fill"></i>
         </a>
         <a href="{{ route('albums') }}" class="text-[#F5F1E6] hover:text-[#d5ccb3] text-[1.5rem] md:text-[2rem] ">
-            <i class="bi bi-disc-fill me-2"></i>
+            <i class="bi bi-disc-fill"></i>
         </a>
-        <a href="{{ route('schedule') }}" class="text-[#B71C1C] hover:text-[#891212] text-[1.5rem] md:text-[2rem] ">
-            <i class="bi bi-calendar-check me-2"></i>
+        {{-- <a href="{{ route('schedule') }}" class="text-[#B71C1C] hover:text-[#891212] text-[1.5rem] md:text-[2rem] ">
+            <i class="bi bi-calendar-check"></i>
+        </a> --}}
+        <a href="{{ route('news') }}" class="text-[#B71C1C] hover:text-[#891212] text-[1.5rem] md:text-[2rem] ">
+            <i class="bi bi-newspaper"></i>
         </a>
         <a href="{{ route('merchandise') }}" class="text-[#B71C1C] hover:text-[#891212] text-[1.5rem] md:text-[2rem] ">
             <i class="bi bi-basket-fill"></i>
+        </a>
     </div>
     <div class="nav-links text-3xl flex justify-center items-center p-6 rounded-lg bg-[#1A1A1B]">
         <a href="{{ route('logout') }}" class="text-[#B71C1C] hover:text-[#891212] text-[1.5rem] md:text-[2rem] ">
