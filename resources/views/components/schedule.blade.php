a0e6e1811bb220d0cb229048b89209e0bd49033f
arga <it.wsm2026@gmail.com>
2026-06-29 10:52:45 +0700

    bandsintown layout

--- resources/views/components/schedule.blade.php
+++ resources/views/components/schedule.blade.php
@@ -1,11 +1,18 @@
 <div class="cover bg-white flex flex-col w-full py-12 justify-center">
-<section class="w-full">
-    <div class="max-w-6xl mx-auto">
-        <script charset="utf-8" src="https://widgetv3.bandsintown.com/main.min.js"></script>
-        <a class="bit-widget-initializer"
-            data-artist-name="id_6784724"
-            data-app-id="2380980eaea24e40832c0c2abc88643d">
-        </a>
-    </div>
-</section>
+    <section class="w-full">
+        <div class="max-w-6xl mx-auto">
+            <script charset="utf-8" src="https://widgetv3.bandsintown.com/main.min.js"></script>
+
+            <a class="bit-widget-initializer" data-artist-name="id_6784724" data-app-id="2380980eaea24e40832c0c2abc88643d"
+                data-display-limit="5">
+            </a>
+
+            <div class="mt-6 text-center">
+                <a href="https://www.bandsintown.com/a/6784724" target="_blank" rel="noopener noreferrer"
+                    class="inline-flex items-center px-5 py-2 rounded-md bg-black text-white">
+                    Read more
+                </a>
+            </div>
+        </div>
+    </section>
 </div>
