d210396413c9f478a20c96f2e2b1fd0607e71f1a
arga <it.wsm2026@gmail.com>
2026-06-30 15:31:11 +0700

    z index adds

--- resources/views/components/banner.blade.php
+++ resources/views/components/banner.blade.php
@@ -1,4 +1,4 @@
-<div class="fixed flex flex-col top-16 w-full justify-center items-center z-50 transition-transform duration-300">
+<div class="absolute flex flex-col top-16 w-full justify-center items-center z-30 transition-transform duration-300">
     <img src="{{ asset('aset/banner/Banner.jpg') }}" loading="lazy" decoding="async"
         alt="whisnu-santika" class="object-cover w-full md:w-[70%] lg:w-[45%] rounded-lg">
 </div>
