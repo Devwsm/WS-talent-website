257e6e0c9804c7f2379fafb3d08998c4ead01177
arga <it.wsm2026@gmail.com>
2026-07-02 12:44:06 +0700

    force

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -18,7 +18,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             accountSeeder::class,
             // scheduleSeeder::class,
-            profileSeeder::class,
+            // profileSeeder::class,
         ]);
     }
 }
