4c899b6f4c2b1160c8e963ee5cd57d9e4fe44edc
arga <it.wsm2026@gmail.com>
2026-07-20 11:22:26 +0700

    home blade restucture

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,33 +1,38 @@
 @extends('template.layout')
 @section('content')
-    <div class="grid grid-cols-1 w-full justify-center items-center">
-        <div class="main-section flex flex-col">
-            <div class="relative">
-                @include('components/navbar')
-                @include('components/banner')
-            </div>
-            <div class="relative">
-                @include('components/videos')
+    @php
+        $color = $color_pages->color ?? '#5E0006';
+    @endphp
+    <div>
+        <div class="grid grid-cols-1 w-full justify-center items-center">
+            <div class="main-section flex flex-col">
+                <div class="relative">
+                    @include('components/navbar')
+                    @include('components/banner')
+                </div>
+                <div class="relative">
+                    @include('components/videos')
+                </div>
             </div>
         </div>
+        <div id="profile" class="profile flex flex-col w-full">
+            @include('components/profile/profile-teaser')
+        </div>
+        <div id="tour" class="schedule flex flex-col w-full">
+            @include('components/schedule')
+        </div>
+        <div id="news" class="news flex flex-col w-full">
+            @include('components/news')
+        </div>
+        <div class="follow bg-white flex flex-col w-full py-12 gap-2 justify-center items-center">
+            <h1 class="capitalize text-md font-bold text-center">Get notified when new events are announced in your area
+            </h1>
+            <button class="bg-white w-fit border-2 border-black px-10 py-4 text-xs uppercase">folow whisnu santika</button>
+        </div>
+        <div class="product flex flex-col w-full">
+            @include('components/albums')
+            @include('components/merchandise')
+        </div>
+        @include('components/footer')
     </div>
-    <div id="profile" class="profile flex flex-col w-full">
-        @include('components/profile/profile-teaser')
-    </div>
-    <div id="tour" class="schedule flex flex-col w-full">
-        @include('components/schedule')
-    </div>
-    <div id="news" class="news flex flex-col w-full">
-        @include('components/news')
-    </div>
-    <div class="follow bg-white flex flex-col w-full py-12 gap-2 justify-center items-center">
-        <h1 class="capitalize text-md font-bold text-center">Get notified when new events are announced in your area</h1>
-        <button class="bg-white w-fit border-2 border-black px-10 py-4 text-xs uppercase">folow whisnu santika</button>
-    </div>
-    <div class="product flex flex-col w-full">
-        @include('components/albums')
-        @include('components/merchandise')
-    </div>
-    @include('components/footer')
-
 @endsection
